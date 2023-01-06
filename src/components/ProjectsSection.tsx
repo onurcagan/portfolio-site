69bd63b2fca25b85e3e615b747b7c0f44931a31b
Onur Çağan <onurcagann@gmail.com>
2023-01-06 17:21:42 +0300

    photos from unsplash used on cards now

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,23 +1,14 @@
 import { Center, Flex } from '@chakra-ui/react'
 import { projectsData } from '../../data/projectsData'
+import { photos } from '../../data/photos'
 import { Card } from './Card'
 
 export const ProjectsSection = () => {
   return (
     <Center>
-      <Flex
-        position={'relative'}
-        justify='center'
-        gap='2rem'
-        px='1rem'
-        pt='5rem'
-        minH='100vh'
-        h='auto'
-        w='100%'
-        flexWrap='wrap'
-      >
-        {projectsData.map((props) => (
-          <Card key={props.projectName} {...props} />
+      <Flex position={'relative'} justify="center" gap="2rem" px="1rem" pt="5rem" minH="100vh" h="auto" w="100%" flexWrap="wrap">
+        {projectsData.map((props, index) => (
+          <Card key={props.projectName} photo={photos[index]} {...props} />
         ))}
       </Flex>
     </Center>
